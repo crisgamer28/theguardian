7f4eaef882babe1f47e967068a6b83662d697147
crisgamer28 <crismilarch@gmail.com>
2025-09-11 16:32:41 -0300

    sensores del enemigo

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 class_name Enemy
 
-@onready var hitbox = $Hitbox
+@onready var hitbox = %HitboxSword
 @onready var left_limit := $left
 @onready var right_limit := $right
 @onready var floor_limit := $floor
@@ -10,34 +10,52 @@ class_name Enemy
 
 @export var speed := 50
 @export var damage := 1
-@export var health = 3
+@export var health = 2
+
 
 var gravity : float = 25.0
 var direction := 1
 var last_position: Vector2 = Vector2.ZERO
 var die_enemy : int = 1
+var jugador = null
+
+enum estados {patrulla, chase}
+var current_states = estados.patrulla
 
 func _ready():
 	animationPlayer.play("walk")
 
 func _physics_process(delta: float) -> void:
-	velocity.x = direction * speed
-
+	match current_states:
+		estados.patrulla:
+				velocity.x = direction * speed
 	# Girar sprite según movimiento
-	sprite2D.flip_h = velocity.x < 0
-	
+				sprite2D.flip_h = velocity.x < 0
+				if direction > 0:
+					hitbox.position.x = abs(hitbox.position.x)
+				else:
+					hitbox.position.x = -abs(hitbox.position.x)
 	# Cambiar de dirección si no hay piso o hay pared
-	if not floor_limit.is_colliding() or left_limit.is_colliding() or right_limit.is_colliding():
-		direction *= -1 
-		var floor_pos = floor_limit.position
-		floor_pos.x *= -1
-		floor_limit.position = floor_pos
-		var right_pos = right_limit.position
-		var left_pos = left_limit.position
-		right_pos.x *= -1
-		left_pos.x *= -1
-		right_limit.position = right_pos
-		left_limit.position = left_pos
+				if not floor_limit.is_colliding() or left_limit.is_colliding() or right_limit.is_colliding():
+					direction *= -1 
+					var floor_pos = floor_limit.position
+					floor_pos.x *= -1
+					floor_limit.position = floor_pos
+					var right_pos = right_limit.position
+					var left_pos = left_limit.position
+					right_pos.x *= -1
+					left_pos.x *= -1
+					right_limit.position = right_pos
+					left_limit.position = left_pos
+		estados.chase:
+			if Player1 != null:
+				var direction_to_player = sign(jugador.global_position.x - global_position.x)
+				velocity.x = direction_to_player * speed
+				sprite2D.flip_h = velocity.x < 0
+				if direction > 0:
+					hitbox.position.x = abs(hitbox.position.x)
+				else:
+					hitbox.position.x = -abs(hitbox.position.x)
 	move_and_slide()
 	last_position = position
 func take_damage(amount : int):
@@ -49,6 +67,9 @@ func take_damage(amount : int):
 		die()
 
 func die():
+	%HitboxSword.queue_free()
+	%Hitboxnormal.queue_free()
+	%Sensor.queue_free()
 	set_physics_process(false)
 	animationPlayer.play("die")
 	await animationPlayer.animation_finished
@@ -68,5 +89,12 @@ func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 		
 
 
-func _on_hitbox_sword_body_entered(body: Node2D) -> void:
-	pass # Replace with function body.
+
+func _on_sensor_body_entered(body: Node2D) -> void:
+	if body is Player1:
+		jugador = body
+		current_states = estados.chase
+func _on_sensor_body_exited(body: Node2D) -> void:
+	if body == jugador:
+		jugador = null
+		current_states = estados.patrulla
--- scripts/game_over_panel.gd
+++ scripts/game_over_panel.gd
@@ -1,16 +1,27 @@
 extends CanvasLayer
 
+@export var scene_menu : PackedScene
 #func _input(event: InputEvent) -> void:
 	#if Input.is_action_pressed("pause"):
 		#visible = not visible
 
-func _on_try_pressed() -> void:
-		get_tree().reload_current_scene()
+#func _on_retry_button_pressed() -> void:
+		#retry_button.get_tree().reload_current_scene()
+		#options_button.get_tree().change_scene_to_file("res://Scenes/menú.tscn")
 
 
-func _on_exit_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/menú.tscn")
+#func _on_options_button_pressed() -> void:
+	#get_tree().change_scene_to_file("res://Scenes/menú.tscn")
 
 
-func _on_point_saved_pressed() -> void:
-	pass
+#func _on_point_saved_pressed() -> void:
+	#pass
+
+
+func _on_retry_button_pressed() -> void:
+	%RetryButton.get_tree().reload_current_scene()
+	
+
+
+func _on_options_button_pressed() -> void:
+	%OptionsButton.get_tree().change_scene_to_packed(scene_menu)
