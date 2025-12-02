521d925f5b3cf542c750324f7b0d168124ffdf90
crisgamer28 <crismilarch@gmail.com>
2025-12-02 17:28:05 -0300

    diálogos chamán

--- Personajes/chamán.gd
+++ Personajes/chamán.gd
@@ -2,26 +2,43 @@ extends CharacterBody2D
 
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var exclamacion: Polygon2D = $Polygon2D
+@onready var exclamacion: Sprite2D = $Exclamación
+
 const BALLOON = preload("uid://q73c4dicnup3")
 const DIALOGO_PRUEBA = preload("uid://ctan7bcod5tk2")
 
 var is_player_close = false
+var dialogo_activo = false
+
+
 func _ready() -> void:
+	self.visible = false
 	animation_player.play("idle")
-	
+	DialogueManager.dialogue_started.connect(_on_dialogue_started)
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
 
-
+func _process(delta: float) -> void:
+	if Player1 and Input.is_action_just_pressed("interactuar") and not dialogo_activo:
+		DialogueManager.show_dialogue_balloon(DIALOGO_PRUEBA, "start")
 
 func _on_hitbox_npc_area_entered(area: Area2D) -> void:
-	if Player1:
-		print("hola")
-	if Player1 and Input.is_action_just_pressed("enter"):
-		DialogueManager.show_dialogue_balloon(DIALOGO_PRUEBA)
 	is_player_close = true
 	exclamacion.visible = true
-
+	self.visible = true
 
 func _on_hitbox_npc_area_exited(area: Area2D) -> void:
+	print("chau")
 	is_player_close = false
 	exclamacion.visible = false
+	self.visible = false
+
+
+
+func _on_dialogue_started(dialogue):
+	dialogo_activo = true
+
+
+
+func _on_dialogue_ended(dialogue):
+	await get_tree().create_timer(0.2).timeout
+	dialogo_activo = false
